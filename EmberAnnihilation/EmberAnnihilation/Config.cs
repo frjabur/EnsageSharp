a7f62f2d150c750a605b90bd15b982da60f483b3
frjabur <frjabur@gmail.com>
2017-06-25 23:55:54 -0300

    Update Config.cs

--- EmberAnnihilation/EmberAnnihilation/Config.cs
+++ EmberAnnihilation/EmberAnnihilation/Config.cs
@@ -1,6 +1,7 @@
 using System;
 using Ensage.Common.Menu;
 using Ensage.SDK.Menu;
+using System.Collections.Generic;
 
 namespace EmberAnnihilation
 {
@@ -14,8 +15,13 @@ namespace EmberAnnihilation
             PussyKey = Factory.Item("Pussy key", new KeyBind('G'));
             AutoChain = Factory.Item("Auto chain in fist", true);
         }
+public MenuItem<AbilityToggler> UseItemsInit { get; }
+public MenuItem<AbilityToggler> UseItems { get; }
 
-        public MenuItem<KeyBind> PussyKey { get; set; }
+public MenuFactory Menu { get; }
+public MenuItem<bool> Enabled { get; }
+public MenuItem<KeyBind> Key { get; }        
+public MenuItem<KeyBind> PussyKey { get; set; }
 
         public MenuItem<bool> AutoChain { get; set; }
 
@@ -25,6 +31,20 @@ namespace EmberAnnihilation
 
         public MenuItem<KeyBind> FistAndComboKey { get; }
 
+public Dictionary<string, bool> Items = new Dictionary<string, bool>
+{
+{ "item_blade_mail", true },
+{ "item_lotus_orb", true },
+{ "item_mjollnir", true },
+{ "item_black_king_bar", false },
+{ "item_shivas_guard", true },
+{ "item_bloodthorn", true },
+{ "item_rod_of_atos", true },
+{ "item_orchid", true },
+{ "item_veil_of_discord", true },
+{ "item_ethereal_blade", true },
+{ "item_dagon", true }
+};
         public void Dispose()
         {
             Factory?.Dispose();
